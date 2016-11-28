571ab2e9a46e27eb5ec6f411cb820467fc58427d
Alco_Rs11 <RIGHTNESSB@GMAIL.COM>
2016-11-27 20:37:35 -0500

    Update Command_ai.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_ai.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_ai.java
@@ -1,42 +1,26 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
-import me.StevenLawson.TotalFreedomMod.Commands.AdminLevel;
-import me.StevenLawson.TotalFreedomMod.Commands.CommandParameters;
-import me.StevenLawson.TotalFreedomMod.Commands.CommandPermissions;
-import me.StevenLawson.TotalFreedomMod.Commands.SourceType;
-import me.StevenLawson.TotalFreedomMod.Commands.TFM_Command;
 import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
-import me.StevenLawson.TotalFreedomMod.TFM_Util;
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
-@CommandParameters(
-        description = "How to become administrator status",
-        aliases = "admininfo,apply",
-        usage = "/<command> [enable|disable]")
-public class Command_ai extends TFM_Command
-{
-    @Override
-    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
-    {
-    
-           
-    
-        TFM_Util.playerMsg(sender_p, "Well, you wish to become administrator, do you?", ChatColor.GOLD);
-        TFM_Util.playerMsg(sender_p, "Well, apply at http://explodingfreedom.boards.net/ and look for something that states 'Admin Applications' and remember to follow the template!", ChatColor.GREEN);
-        TFM_Util.playerMsg(sender_p, "Don't beg to look at the application, it'll shorten the percentage on if it can get accepted.", ChatColor.GOLD);
-        TFM_Util.playerMsg(sender_p, "How to apply for admin", ChatColor.GOLD);
-        TFM_Util.playerMsg(sender_p, "To apply for admin you need to go to the forums at http://explodingfreedom.boards.net", ChatColor.GOLD);
-        TFM_Util.playerMsg(sender_p, "Then if you feel you are ready, make a new thread in the 'admin applications' board.", ChatColor.GOLD);
-        TFM_Util.playerMsg(sender_p, "And fill out the template in the new thread.", ChatColor.GOLD);
-        TFM_Util.playerMsg(sender_p, "Please dont nag admins for recs, this will weaken your chances for Super Admin.", ChatColor.RED);
-        TFM_Util.playerMsg(sender_p,"Good Luck And Have A GREAT Day At EXPLODINGFreedom!", ChatColor.GOLD);
-       
-        return true;
-       }
-     
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "How to become administrator status", aliases = "ai", usage = "/<command>")
+public class Command_ai extends TFM_Command {
+      @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole) {
+        sender.sendMessage(ChatColor.GOLD+ "§b=========§8[&bAdmin Application Instructions§8]§b=======");
+        sender.sendMessage(ChatColor.GREEN + "§e- §c Go to '§bhttp://explodingfreedom.boards.net/§e' and make an account. 'Admin Applications' and remember to follow the template!");
+        sender.sendMessage(ChatColor.GOLD + "§e- §c Don't beg to look at the application, it'll shorten the percentage on if it can get accepted.");
+        sender.sendMessage(ChatColor.GOLD + "§e- §c How to apply for admin");
+        sender.sendMessage(ChatColor.GOLD + "§e- §c To apply for admin you need to meet the requirements found at '§bhttp://explodingfreedom.boards.net/thread/294/admin-application-requirements-policy'");
+        sender.sendMessage(ChatColor.GOLD + "§e- §cOnce you meet the requirements, go to a section titled 'Super Admin Applications'.");
+        sender.sendMessage(ChatColor.GOLD + "§e- §cFill out the template in the new thread.");
+        sender.sendMessage(ChatColor.RED + "§e- §c Please dont nag admins for recommendations or vouches, this will weaken your chances for Super Admin.");
+        sender.sendMessage(ChatColor.GOLD + "That's all to it! Good luck!");
+             return true;
+
+    }
 }
